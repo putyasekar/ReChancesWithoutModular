c388e89ed924e071a59c8ea2d05b55adde59a484
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2021-10-26 09:29:01 +0700

    rebuild kesekian

---
+++ app/src/main/java/com/putya/rechanceswithoutmodular/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.putya.rechanceswithoutmodular
+
+class LoginActivity {
+}
---
+++ app/src/main/java/com/putya/rechanceswithoutmodular/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.putya.rechanceswithoutmodular
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(5000L)
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }
+    }
+}
