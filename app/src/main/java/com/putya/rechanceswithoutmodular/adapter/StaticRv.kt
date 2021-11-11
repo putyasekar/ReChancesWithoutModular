30c6b0f9bdacc2ea457a28cf120f7403a757c120
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2021-11-11 19:59:29 +0700

    11 xml home page done

---
+++ app/src/main/java/com/putya/rechanceswithoutmodular/adapter/StaticRv.kt
@@ -0,0 +1,4 @@
+package com.putya.rechanceswithoutmodular.adapter
+
+class StaticRv {
+}
---
+++ app/src/main/java/com/putya/rechanceswithoutmodular/model/StaticRvList.kt
@@ -0,0 +1,7 @@
+package com.putya.rechanceswithoutmodular.model
+
+import androidx.appcompat.app.AppCompatActivity
+
+class StaticRvList : AppCompatActivity(
+//    var display: Int, var text: String
+)
