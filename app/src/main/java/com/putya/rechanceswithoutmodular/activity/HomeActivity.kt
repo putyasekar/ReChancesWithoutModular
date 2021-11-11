7e2b115c4be6bfe5800f61783477d943448b41c8
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2021-11-11 11:58:18 +0700

    9 cleaning

--- app/src/main/java/com/putya/rechanceswithoutmodular/activity/HomeActivity.kt
+++ app/src/main/java/com/putya/rechanceswithoutmodular/activity/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.putya.rechanceswithoutmodular.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.putya.rechanceswithoutmodular.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
