c39041fc8cf48ac6831ce083fdddfebe269d4d76
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2021-11-16 09:34:23 +0700

    31

---
+++ app/src/main/java/com/putya/rechanceswithoutmodular/activity/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.putya.rechanceswithoutmodular.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.putya.rechanceswithoutmodular.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
