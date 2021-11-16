3d68fa7e522b50b011fcdf8c8f0d759d665712c7
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2021-11-16 20:22:16 +0700

    32 UI Supports

--- app/src/main/java/com/putya/rechanceswithoutmodular/activity/ChatActivity.kt
+++ app/src/main/java/com/putya/rechanceswithoutmodular/activity/ChatActivity.kt
@@ -2,6 +2,7 @@ package com.putya.rechanceswithoutmodular.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.putya.rechanceswithoutmodular.R
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
