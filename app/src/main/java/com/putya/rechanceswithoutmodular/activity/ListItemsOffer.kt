5a8df6b47d9f97d2f7ea0c6710f8ebd7ecc9b6cd
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2021-11-19 14:54:28 +0700

    47

---
+++ app/src/main/java/com/putya/rechanceswithoutmodular/activity/ListItemsOffer.kt
@@ -0,0 +1,12 @@
+package com.putya.rechanceswithoutmodular.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.putya.rechanceswithoutmodular.R
+
+class ListItemsOffer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_items_offer)
+    }
+}
