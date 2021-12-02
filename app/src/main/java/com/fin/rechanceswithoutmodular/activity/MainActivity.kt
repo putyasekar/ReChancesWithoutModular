f0d0bdaada8b8590f853b36fe49f037b6ef8782b
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2021-12-02 21:25:11 +0700

    96

--- app/src/main/java/com/fin/rechanceswithoutmodular/activity/MainActivity.kt
+++ app/src/main/java/com/fin/rechanceswithoutmodular/activity/MainActivity.kt
@@ -2,41 +2,43 @@ package com.fin.rechanceswithoutmodular.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import com.fin.rechanceswithoutmodular.R
 import com.fin.rechanceswithoutmodular.fragment.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val onNavigationItemSelectedListener =
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
+    private val setOnItemSelectedListener =
+        ChipNavigationBar.OnItemSelectedListener setOnItemSelectedListener@{ id ->
+            when (id) {
                 R.id.home_page -> {
                     val homeFragment = HomeFragment()
                     addFragment(homeFragment)
-                    return@OnNavigationItemSelectedListener true
+                    return@ true
                 }
                 R.id.list_item_page -> {
                     val bookingFragment = ListItemFragment()
                     addFragment(bookingFragment)
-                    return@OnNavigationItemSelectedListener true
+                    return@setOnItemSelectedListener true
                 }
                 R.id.post_page -> {
                     val inboxFragment = PostFragment()
                     addFragment(inboxFragment)
-                    return@OnNavigationItemSelectedListener true
+                    return@setOnItemSelectedListener true
                 }
                 R.id.chat_page -> {
                     val historyFragment = ChatFragment()
                     addFragment(historyFragment)
-                    return@OnNavigationItemSelectedListener true
+                    return@setOnItemSelectedListener true
                 }
                 R.id.profile_page -> {
                     val profileFragment = ProfileFragment()
                     addFragment(profileFragment)
-                    return@OnNavigationItemSelectedListener true
+                    return@setOnItemSelectedListener true
                 }
             }
             false
@@ -55,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //WAIT!!!
-//        chip_nav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        chip_nav.setOnItemSelectedListener(ChipNavigationBar.OnItemSelectedListener)
 
         addFragment(defaultMainView)
 
