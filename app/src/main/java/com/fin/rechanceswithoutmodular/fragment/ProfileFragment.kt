4e29b939ddb9715df223775c36ef1f7e51ad847f
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2021-12-06 09:32:18 +0700

    106 dark mode, forgot to commit

--- app/src/main/java/com/fin/rechanceswithoutmodular/fragment/ProfileFragment.kt
+++ app/src/main/java/com/fin/rechanceswithoutmodular/fragment/ProfileFragment.kt
@@ -1,19 +1,39 @@
 package com.fin.rechanceswithoutmodular.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import com.fin.rechanceswithoutmodular.R
+import com.fin.rechanceswithoutmodular.databinding.FragmentChatBinding
+import com.fin.rechanceswithoutmodular.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-
+    lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.switch1.setOnCheckedChangeListener { _, isChecked ->
+
+            if (binding.switch1.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.switch1.text = "Disable dark mode"
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.switch1.text = "Enable dark mode"
+            }
+        }
     }
 }
